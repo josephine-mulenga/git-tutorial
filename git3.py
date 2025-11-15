6e8a3e01f5d5899e8827c850a032519531b964cc
Josephine Mulenga <chishimbaaa06@gmail.com>
2025-11-15 05:44:09 -0500

    updated git3.py

--- git3.py
+++ git3.py
@@ -3,6 +3,10 @@ print("hello", name)
 x = 60
 if x < 10:
     print ("less that 10")
+elif 10<= x <25:
+    print ("The value is a double digit")
+elif x>25:
+    print( "greater than 25")
 else:
     print ("end")
     
